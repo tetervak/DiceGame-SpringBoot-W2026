7bd41b9e98cee77c7653923636b28200bbc1da6b
Alex Tetervak <tetervak@sheridancollege.ca>
2026-01-13 08:42:11 -0500

    initial commit

---
+++ src/test/java/ca/tetervak/dicegame/DiceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tetervak.dicegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
